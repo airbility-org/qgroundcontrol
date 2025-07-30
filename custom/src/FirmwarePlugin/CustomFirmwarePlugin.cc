70f471bc8ad6684b1213138f4534894770bea2c3
tichall <0817@gmail.com>
2025-07-30 11:40:57 +0900

    [fix] #8 Unhide several modes by updating settings

--- custom/src/FirmwarePlugin/CustomFirmwarePlugin.cc
+++ custom/src/FirmwarePlugin/CustomFirmwarePlugin.cc
@@ -19,13 +19,7 @@
 //-----------------------------------------------------------------------------
 CustomFirmwarePlugin::CustomFirmwarePlugin()
 {
-    for (auto &mode: _flightModeList){
-        //-- Narrow the flight mode options to only these
-        if(mode.mode_name != _holdFlightMode && mode.mode_name != _rtlFlightMode && mode.mode_name != _missionFlightMode){
-            // No other flight modes can be set
-            mode.canBeSet = false;
-        }
-    }
+
 }
 
 //-----------------------------------------------------------------------------
